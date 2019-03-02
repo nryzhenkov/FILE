86b273530e210bf0e485974ad7de5614a9de0162
nryzhenkov <nryzhenkov@yandex.ru>
2019-03-02 23:07:39 +0300

    test

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine("Название: {0}", drive.Name);   
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
